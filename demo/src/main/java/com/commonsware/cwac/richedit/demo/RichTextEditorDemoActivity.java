c5e5a3a0a0e41ff9fdbece272773c56bba4c54a5
Jakub Krolewski <jakub.krolewski@allegrogroup.com>
2015-06-03 11:16:28 +0200

    [feature/adding_toolbar_to_demo] Adding toolbar to the demo app

--- demo/src/main/java/com/commonsware/cwac/richedit/demo/RichTextEditorDemoActivity.java
+++ demo/src/main/java/com/commonsware/cwac/richedit/demo/RichTextEditorDemoActivity.java
@@ -15,21 +15,34 @@
 package com.commonsware.cwac.richedit.demo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.text.Selection;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 import com.commonsware.cwac.colormixer.ColorMixerActivity;
-import com.commonsware.cwac.richedit.ColorPicker;
-import com.commonsware.cwac.richedit.ColorPickerOperation;
-import com.commonsware.cwac.richedit.RichEditText;
-import com.commonsware.cwac.richedit.URLEffect;
+import com.commonsware.cwac.richedit.*;
+import com.commonsware.cwac.richtextutils.SpannedXhtmlGenerator;
+
+import java.util.List;
 
 public class RichTextEditorDemoActivity extends Activity
   implements ColorPicker {
   private static final int COLOR_REQUEST=1337;
   private RichEditText editor=null;
+  private ToggleButton boldToggleButton;
+  private ToggleButton italicToggleButton;
+  private ToggleButton fontSizeToggleButton;
+  private ToggleButton alignmentToggleButton;
+  private ToggleButton bulletToggleButton;
+  private Button htmlPreviewButton;
   private ColorPickerOperation colorPickerOp=null;
   
   @Override
@@ -39,8 +52,92 @@ public class RichTextEditorDemoActivity extends Activity
     setContentView(R.layout.main);
     
     editor=(RichEditText)findViewById(R.id.editor);
+    boldToggleButton=(ToggleButton)findViewById(R.id.bold);
+    italicToggleButton=(ToggleButton)findViewById(R.id.italic);
+    fontSizeToggleButton=(ToggleButton)findViewById(R.id.fontSize);
+    alignmentToggleButton=(ToggleButton)findViewById(R.id.alignment);
+    bulletToggleButton=(ToggleButton)findViewById(R.id.bullet);
+    htmlPreviewButton=(Button)findViewById(R.id.htmlPreview);
+
     editor.setColorPicker(this);
     editor.enableActionModes(true);
+    editor.setOnSelectionChangedListener(new RichEditText.OnSelectionChangedListener() {
+      @Override
+      public void onSelectionChanged(int start, int end, List<Effect<?>> effects) {
+        Log.d("DemoActivity", "onSelectionChanged, start: " + start + ", end: " + end + ", effects: " + effects);
+        boolean boldEnabled = false;
+        boolean italicEnabled = false;
+        boolean fontSizeChanged = false;
+        boolean alignmentChanged = false;
+        boolean bulletEnabled = false;
+
+        for (Effect effect : effects) {
+          if (effect == RichEditText.BOLD) {
+            boldEnabled = true;
+          } else if (effect == RichEditText.ITALIC) {
+            italicEnabled = true;
+          } else if (effect == RichEditText.LINE_ALIGNMENT) {
+            alignmentChanged = effect.valueInSelection(editor).equals(Layout.Alignment.ALIGN_OPPOSITE);
+          } else if (effect == RichEditText.BULLET) {
+            bulletEnabled = true;
+          } else if (effect == RichEditText.RELATIVE_SIZE) {
+            fontSizeChanged = effect.valueInSelection(editor).equals(1.5f);
+          }
+        }
+
+        boldToggleButton.setChecked(boldEnabled);
+        italicToggleButton.setChecked(italicEnabled);
+        fontSizeToggleButton.setChecked(fontSizeChanged);
+        alignmentToggleButton.setChecked(alignmentChanged);
+        bulletToggleButton.setChecked(bulletEnabled);
+      }
+    });
+
+    boldToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        editor.applyEffect(RichEditText.BOLD, isChecked);
+      }
+    });
+
+    italicToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        editor.applyEffect(RichEditText.ITALIC, isChecked);
+      }
+    });
+
+    fontSizeToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        editor.applyEffect(RichEditText.RELATIVE_SIZE, isChecked ? 1.5f : 1f);
+      }
+    });
+
+    alignmentToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        editor.applyEffect(RichEditText.LINE_ALIGNMENT,
+                isChecked ? Layout.Alignment.ALIGN_OPPOSITE : Layout.Alignment.ALIGN_NORMAL);
+      }
+    });
+
+    bulletToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        editor.applyEffect(RichEditText.BULLET, isChecked);
+      }
+    });
+
+    htmlPreviewButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        new AlertDialog.Builder(RichTextEditorDemoActivity.this)
+                .setMessage(new SpannedXhtmlGenerator().toXhtml(editor.getText()))
+                .setPositiveButton(android.R.string.ok, null)
+                .show();
+      }
+    });
   }
 
   @Override
